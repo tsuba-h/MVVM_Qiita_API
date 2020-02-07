e74b5dcd522dd56f4694282b663e2ef606694113
服部　翼 <tsuba-h@arsaga.jp>
2020-02-07 18:37:28 +0900

    Initial Commit

---
+++ MVVM_GithubAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM_GithubAPI
+//
+//  Created by 服部　翼 on 2020/02/07.
+//  Copyright © 2020 服部　翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
