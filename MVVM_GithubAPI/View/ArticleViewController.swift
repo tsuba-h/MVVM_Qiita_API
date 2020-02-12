edbc6cfc1331990c30efc513d1e10c040ce015c7
Unknown <tsuba-h@arsaga.jp>
2020-02-12 16:16:14 +0900

    記事用のvc作成

---
+++ MVVM_GithubAPI/View/ArticleViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ArticleViewController.swift
+//  MVVM_GithubAPI
+//
+//  Created by 服部　翼 on 2020/02/12.
+//  Copyright © 2020 服部　翼. All rights reserved.
+//
+
+import UIKit
+import Instantiate
+import InstantiateStandard
+
+class ArticleViewController: UIViewController, StoryboardInstantiatable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
